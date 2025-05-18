8fd7571447b21256774e867c6760246e131cb1da
Kavishka <kavishka@echannelling.oom>
2025-05-18 21:27:45 +0530

    fix image not showing issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,13 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1>Bio Lab</h1>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/biolabtestlogo.png"
-          alt="biolabtestlogo"
-          width={180}
-          height={38}
-          priority
-        />
+          <img
+              className="dark:invert"
+              src="/biolabtestlogo.png"
+              alt="biolabtestlogo"
+              width={180}
+              height={38}
+          />
       </main>
     </div>
   );
